59d83f180207ada761512db505f8fe19ea2b0bbd
Arjunsing1997 <acchur1997@gmail.com>
2021-04-29 19:47:37 +0530

    UC5 Double max number

--- GenericPracticeProblem/GenericPracticeProblem/Program.cs
+++ GenericPracticeProblem/GenericPracticeProblem/Program.cs
@@ -54,7 +54,32 @@ namespace GenericPracticeProblem
             return first;
 
         }
-        
+        public double MaxDoubleNumber(double first, double second, double third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+               first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+               first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            {
+                return third;
+            }
+            return first;
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ----------GENERIC PROBLEMS-----------------");
