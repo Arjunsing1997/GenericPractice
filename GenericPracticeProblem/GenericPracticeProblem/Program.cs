547c219917b42d45628dc5ac622fa2f9e3534967
Arjunsing1997 <acchur1997@gmail.com>
2021-04-29 16:09:47 +0530

    UC1-Print Array

--- GenericPracticeProblem/GenericPracticeProblem/Program.cs
+++ GenericPracticeProblem/GenericPracticeProblem/Program.cs
@@ -8,9 +8,50 @@ namespace GenericPracticeProblem
 {
     class Program
     {
+        public static void IntegerArray(int[] array)
+        {
+            foreach(int item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------");
+        }
+
+        public static void DoubleArray(double[] array)
+        {
+            foreach (double item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------");
+        }
+
+        public static void CharArray(char[] array)
+        {
+            foreach (char item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ----------GENERIC PROBLEMS-----------------");
+
+            //Creating and Assigning Values to array
+            int[] intArray = { 10, 20, 30 };
+            double[] doubleArray = { 10.10, 20.20, 30.30 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            //Calling Methods in Main Mehtod
+            Program.IntegerArray(intArray);
+            Program.DoubleArray(doubleArray);
+            Program.CharArray(charArray);
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
