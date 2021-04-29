fab635a564558083f69001c9ce869a2156348099
Arjunsing1997 <acchur1997@gmail.com>
2021-04-29 15:45:49 +0530

    Welcome Message

---
+++ GenericPracticeProblem/GenericPracticeProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ----------GENERIC PROBLEMS-----------------");
+        }
+    }
+}
