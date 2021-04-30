0735a469ed4031670497b857cd7bb82b10b9bb92
Arjunsing1997 <acchur1997@gmail.com>
2021-04-30 15:57:50 +0530

    UC8 Standard Output

--- GenericPracticeProblem/GenericPracticeProblem/Program.cs
+++ GenericPracticeProblem/GenericPracticeProblem/Program.cs
@@ -14,64 +14,49 @@ namespace GenericPracticeProblem
         /// <typeparam name="T"></typeparam>
         public class PrintArray<T> where T : IComparable
         {
-            public T first, second, third;
-            public PrintArray(T first, T second, T third)//Creating an Constructor
+            public T[] value;
+            public PrintArray(T[] value)//Creating an Constructor
             {
-                this.first = first;
-                this.second = second;
-                this.third = third;
+                this.value = value;
             }
-            public static T MaxValues(T first, T second, T third)
+
+            public T[] Sort(T[] value)
             {
-                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
-                   first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
-                   first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
-                {
-                    return first;
-                }
-
-                if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
-                   second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
-                   second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
-                {
-                    return second;
-                }
-
-                if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
-                   third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
-                   third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
-                {
-                    return third;
-                }
-                return first;
+                Array.Sort(value);  //dorting an Array
+                return value;
+            }
+            public T MaxValues(T[] value)
+            {
+                var sorted_values = Sort(value);    //calling sort method to sort an array
+                return sorted_values[1];
             }
             public T MaxMethod()
             {
-                T max = PrintArray<T>.MaxValues(this.first, this.second, this.third);
+                var max = MaxValues(this.value); //calling MaxValue method and passing values to it
                 return max;
             }
+            public void PrintValues()
+            {
+                var max = MaxValues(this.value);
+                Console.WriteLine("Maximum value is " + max);
+            }
         }
-
-        
-
-
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ----------GENERIC PROBLEMS-----------------");
 
             //Creating and Assigning Values to array
-            //int[] intArray = { 10, 20, 30 };
-            //double[] doubleArray = { 10.10, 20.20, 30.30 };
-            //char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            int[] intArray = { 10, 20, 30, 50 };
+            double[] doubleArray = { 10.10, 20.20, 30.30 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
 
-            PrintArray<int> generic1 = new PrintArray<int>(10, 20, 30);
-            Console.WriteLine("Integer Max Number--->"+generic1.MaxMethod());
-            PrintArray<double> generic2 = new PrintArray<double>(10.4, 20.6, 30.1);
-            Console.WriteLine("Double Max Number--->"+generic2.MaxMethod());
-            PrintArray<char> generic3 = new PrintArray<char>('a','b','c');
-            Console.WriteLine("Charecter Max Letter--->"+generic3.MaxMethod());
-
+            PrintArray<int> generic1 = new PrintArray<int>(intArray);   //creating an Object
+            generic1.PrintValues();     //Calling a method using an Object
+            PrintArray<double> generic2 = new PrintArray<double>(doubleArray);
+            generic2.PrintValues();
+            PrintArray<char> generic3 = new PrintArray<char>(charArray);
+            generic3.PrintValues();
             Console.ReadLine();
         }
     }
