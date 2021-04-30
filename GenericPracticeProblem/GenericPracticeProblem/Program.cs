7f1d5f79e1c69db4dab77e152d30140db7b520e6
Arjunsing1997 <acchur1997@gmail.com>
2021-04-30 10:35:46 +0530

    UC6-maximun Char

--- GenericPracticeProblem/GenericPracticeProblem/Program.cs
+++ GenericPracticeProblem/GenericPracticeProblem/Program.cs
@@ -79,6 +79,31 @@ namespace GenericPracticeProblem
             return first;
 
         }
+        public string MaxChar(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+               first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+               first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+               second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+               third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+            {
+                return third;
+            }
+            return first;
+
+        }
 
         static void Main(string[] args)
         {
--- GenericPracticeProblem/ManIntegerNumberTest/UnitTest1.cs
+++ GenericPracticeProblem/ManIntegerNumberTest/UnitTest1.cs
@@ -8,12 +8,36 @@ namespace ManIntegerNumberTest
     public class UnitTest1
     {
         [TestMethod]
+        public void MaxIntegerNumber()
+        {
+            Program p = new Program();
+            int actual = p.MaxIntNumber(10, 15, 14);
+
+            int Exp = 15;
+
+            Assert.AreEqual(actual, Exp);
+
+        }
+
+        [TestMethod]
         public void MaxDoubleNumber()
         {
             Program p = new Program();
             double actual = p.MaxDoubleNumber(10.6, 10.0, 14.6);
 
             double Exp = 14.6;
+
+            Assert.AreEqual(actual, Exp);
+
+        }
+
+        [TestMethod]
+        public void MaxStringNumber()
+        {
+            Program p = new Program();
+            string actual = p.MaxChar("abc","cde","xyz");
+
+            string Exp = "xyz";
 
             Assert.AreEqual(actual, Exp);
 
